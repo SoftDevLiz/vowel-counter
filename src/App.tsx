43aade8a894b4aa6e5584cab94ec350bd625e2b1
Liezl Mouton <liz.mouton1@gmail.com>
2025-07-22 17:59:11 +0200

    Create basic skeleton of the page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
-
+import { Textarea } from "./components/ui/textarea"
+import "./tailwind.css"
 
 function App() {
 
   return (
     <>
-      <div>
+      <div className="flex flex-col p-10 items-center justify-center">
+        <h1 className="text-5xl">Vowel Counter</h1>
+        <h2 className="text-xl p-5">Enter your text below</h2>
       </div>
+      <div className="flex flex-col items-center justify-center">
+          <Textarea placeholder="start typing..." className="max-w-[250px]"/>
+      </div>
+      <div className="flex items-center justify-center p-10 text-5xl">
+        <h1>5</h1>
+      </div>
+
     </>
   )
 }
