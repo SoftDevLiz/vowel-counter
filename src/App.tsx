b81b5f1a5ed8cb09b0ddaaa7efc2a74c3a26a69f
Liezl Mouton <liz.mouton1@gmail.com>
2025-07-25 16:41:01 +0200

    Center the app on the page

--- src/App.tsx
+++ src/App.tsx
@@ -2,38 +2,32 @@ import { useState } from "react";
 import VowelCounter from "./components/VowelCounter"
 import { Textarea } from "./components/ui/textarea"
 import "./tailwind.css"
+import { Button } from "./components/ui/button";
 
 function App() {
 const [text, setText] = useState("");
 
   return (
-    <>
-      <div className="flex flex-col p-10 items-center justify-center">
+    <div className="flex flex-col min-h-screen items-center justify-evenly">
+      <div className="flex flex-col items-center gap-5">
         <h1 className="text-5xl">Vowel Counter</h1>
-        <h2 className="text-xl p-5">Enter your text below</h2>
+        <h2 className="text-xl">Enter your text below</h2>
       </div>
-      <div className="flex flex-col items-center justify-center">
-          <Textarea placeholder="start typing..." className="max-w-[250px]" onChange={event => setText(event.target.value)}/>
-      </div>
-      <div className="flex items-center justify-center p-10 text-5xl">
+      <div className="text-5xl">
         <VowelCounter 
         string={text}
         />
       </div>
-
-    </>
+      <div className="flex flex-col items-center gap-5">
+          <Textarea placeholder="start typing..." value={text} onChange={event => setText(event.target.value)} />
+          <Button onClick={() => setText("")}>Clear</Button>
+      </div>
+    </div>
   )
 }
 
 export default App
 
 // TODO:
-// I want it to live update as you type :)
-// 1. Abstract counter component...
-// 2. Create counter functionality
-// 3. Create reset button
-// 4. Make it mobile friendly
-// 5. Style the front end!
-// Bonus: How do we make a cool animation as it counts while you type? I'm thinking a black background and big white letters that spreads over the page
-// what would be REALLY cool if the letters were from what the user has typed... so as you type the letters start creeping onto the page from the bottom up
-// and as you delete them they dissapear from the page.
+// Center the app on the page screen
+// We want the number in the middle and the input/reset button below
