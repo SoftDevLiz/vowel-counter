f479028c9ef8dc705740febcb407b0a97b982c2a
Liezl Mouton <liz.mouton1@gmail.com>
2025-07-26 14:28:49 +0200

    Start implementing logic for letter rain effect

--- src/App.tsx
+++ src/App.tsx
@@ -5,19 +5,27 @@ import UserInput from "./components/UserInput";
 import { useState } from "react";
 
 function App() {
+
+  // useState for VowelCounter
   const [text, setText] = useState("");
+
+  // useState for LetterRain
+  const [letters, setLetters] = useState<{ id: string; char: string; left: number; duration: number }[]>([]);
+
+
+
+
 
   return (
     <div className="flex flex-col min-h-screen items-center justify-evenly px-4 sm:px-8 bg-zinc-900 text-white">
       <Header />
       <VowelCounter text={text}/>
-      <UserInput text={text} setText={setText} />
+      <UserInput text={text} setText={setText} setLetters={setLetters} />
     </div>
   )
 }
 
 export default App
 
-// TODO:
 // Typing animation...
 // Styling
--- src/components/UserInput.tsx
+++ src/components/UserInput.tsx
@@ -4,16 +4,40 @@ import { Textarea } from './ui/textarea';
 type UserInputProps = {
     text: string,
     setText: React.Dispatch<React.SetStateAction<string>>,
+    setLetters: React.Dispatch<React.SetStateAction<{ id: string; char: string; left: number; duration: number }[]>>,
 }
 
-const UserInput = ({text, setText}: UserInputProps) => {
+const UserInput = ({text, setText, setLetters}: UserInputProps) => {
+
+// Change event for LetterRain component
+const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    const value = e.target.value;
+    const lastChar = value.slice(-1);
+
+    if (lastChar) {
+        const newLetter = {
+
+        id: Date.now().toString(),
+        char: lastChar,
+        left: Math.random() * 90,
+        duration: Math.random() * 2,
+
+    };
+
+    setLetters((prev) => [...prev, newLetter])
+
+    }
+}
 
     return (
         <div className="flex flex-col items-center gap-4 sm:gap-5 w-full max-w-md">
             <Textarea 
                 placeholder="start typing..." 
                 value={text} 
-                onChange={event => setText(event.target.value)}
+                onChange={event => {
+                    setText(event.target.value);
+                    handleChange(event);
+                }}
                 className="w-full" 
             />
             <Button onClick={() => setText("")}>Clear</Button>
